7eb31f220ec3a6e2b025cbb27be0086b9bb883b3
daffasatrianegara <daffasatria036@gmail.com>
2024-11-07 23:31:38 +0700

    add new file

--- 08 nodejs-web-server/server2.js
+++ 08 nodejs-web-server/server2.js
@@ -5,14 +5,18 @@ const host = String("localhost");
 const requestHandler = (req, res) => {
   //   res.setHeader("Content-Type", "text/html"); // jika menggunakan ini maka browser akan render response menjadi bentuk html
   res.setHeader("Content-Type", "application/json"); // render dalam bentuk json
-  res.setHeader('X-Powered-By', 'NodeJS'); // memberi tahu client mesin apa yang digunakan
+  res.setHeader("X-Powered-By", "NodeJS"); // memberi tahu client mesin apa yang digunakan
   res.statusCode = 200;
 
   const { method, url } = req;
   if (url === "/") {
     if (method === "GET") {
       res.statusCode = 200;
-      res.end("<h1>Hello world!</h1>");
+      res.end(
+        JSON.stringify({
+          message: "ini adalah halaman Home.",
+        })
+      );
     } else if (method === "POST") {
       let body;
 
@@ -24,25 +28,43 @@ const requestHandler = (req, res) => {
         body = Buffer.concat(body).toString();
         const { name, age } = JSON.parse(body);
         res.statusCode = 200;
-        res.end(`Hallo ${name}, kamu memiliki umur ${age}`);
+        res.end(
+          JSON.stringify({
+            message: `Hallo ${name}, kamu memiliki umur ${age}.`,
+          })
+        );
       });
     } else {
       res.statusCode = 400;
-      res.end(`method ${method} tidak diketahui`);
+      res.end(
+        JSON.stringify({
+          message: `method ${method} tidak diketahui.`,
+        })
+      );
     }
   } else if (url === "/about") {
     if (method === "GET") {
       res.statusCode = 200;
       res.end(
-        "<h1>kami adalah perusahaan yang bergerak dibidang teknologi!</h1>"
+        JSON.stringify({
+          message: "kami adalah perusahaan yang bergerak dibidang teknologi!",
+        })
       );
     } else {
       res.statusCode = 400;
-      res.end(`Method ${method} tidak diketahui`);
+      res.end(
+        JSON.stringify({
+          message: `Method ${method} tidak diketahui`,
+        })
+      );
     }
   } else {
     res.statusCode = 404;
-    res.end("<h1>url tidak ditemukan...</h1>");
+    res.end(
+      JSON.stringify({
+        message: "url tidak ditemukan...",
+      })
+    );
   }
 };
 
